74ea919fc47109a49c4cb711f0e98428f82f4137
Matt <mattluo604@hotmail.com>
2021-04-10 22:54:12 +0000

    new tinker

--- tinkering.js
+++ tinkering.js
@@ -1,9 +1,17 @@
-const whichSchool  = function (age) {
-  // Your code in here ...
-  if(age < 13){
-    console.log("Elementary School")
-  }else if( 13 <= age && age <= 18 ) {
-    console.log("Secondary School")
-  }else{ console.log("Lighthouse Labs")
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north'] 
+let x = 0
+let y = 0
+function finalPosition(moves){
+  for(let move of moves){
+  if(move === 'north'){
+  y += 1;
+      }else if ( move === 'east'){
+         x += 1;
+      }else if ( move === 'south'){
+         y -= 1;
+      }else if ( move === 'west'){
+         x -= 1;
+      }
+    }return[x,y];
   }
-};
+  console.log(finalPosition(moves));
