7fcaa983401c189ecffee93c6ac80ea238ade5c4
Matt <mattluo604@hotmail.com>
2021-04-12 00:15:28 +0000

    range

---
+++ range.js
@@ -0,0 +1,11 @@
+function range(start, end, step){
+  let array = [];
+  while(start <= end){
+    array.push(start);
+    start += step;
+  }
+  return array;
+}
+console.log(range(0, 10, 2));
+console.log(range(10, 30, 5));
+console.log(range(-5, 2, 3));
